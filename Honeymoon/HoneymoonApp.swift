cc5412d6c1d70d5fac79c3a403e59d16187419f6
Marygimm <marygimm.moreira@bringglobal.com>
2022-10-27 10:19:22 +0000

    add assets sounds and start honeymoon App

---
+++ Honeymoon/HoneymoonApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoneymoonApp.swift
+//  Honeymoon
+//
+//  Created by Mary Moreira on 27/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct HoneymoonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
