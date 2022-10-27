80dafb58cac5e43d15a5dfb2013ac2620fda0aa3
Marygimm <marygimm.moreira@bringglobal.com>
2022-10-27 10:23:05 +0000

    create model and data for honeymoon

---
+++ Honeymoon/Model/HoneymoonModel.swift
@@ -0,0 +1,14 @@
+//
+//  HoneymoonModel.swift
+//  Honeymoon
+//
+//  Created by Mary Moreira on 27/10/2022.
+//
+
+import SwiftUI
+
+struct Destination {
+  var place: String
+  var country: String
+  var image: String
+}
