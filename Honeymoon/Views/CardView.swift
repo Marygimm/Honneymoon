4087f2f660c2e935e44d8a92135bba80370bba44
Marygimm <marygimm.moreira@bringglobal.com>
2022-10-27 10:23:12 +0000

    create card view

--- Honeymoon/Views/CardView.swift
+++ Honeymoon/Views/CardView.swift
@@ -1,5 +1,5 @@
 //
-//  SwiftUIView.swift
+//  CardView.swift
 //  Honeymoon
 //
 //  Created by Mary Moreira on 27/10/2022.
@@ -7,14 +7,54 @@
 
 import SwiftUI
 
-struct SwiftUIView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+struct CardView: View, Identifiable {
+  // MARK: - PROPERTIES
+  
+  let id = UUID()
+  var honeymoon: Destination
+  
+  var body: some View {
+    Image(honeymoon.image)
+      .resizable()
+      .cornerRadius(24)
+      .scaledToFit()
+      .frame(minWidth: 0, maxWidth: .infinity)
+      .overlay(
+        VStack(alignment: .center, spacing: 12) {
+          Text(honeymoon.place.uppercased())
+            .foregroundColor(Color.white)
+            .font(.largeTitle)
+            .fontWeight(.bold)
+            .shadow(radius: 1)
+            .padding(.horizontal, 18)
+            .padding(.vertical, 4)
+            .overlay(
+              Rectangle()
+                .fill(Color.white)
+                .frame(height: 1),
+              alignment: .bottom
+          )
+          Text(honeymoon.country.uppercased())
+            .foregroundColor(Color.black)
+            .font(.footnote)
+            .fontWeight(.bold)
+            .frame(minWidth: 85)
+            .padding(.horizontal, 10)
+            .padding(.vertical, 5)
+            .background(
+              Capsule().fill(Color.white)
+            )
+        }
+        .frame(minWidth: 280)
+        .padding(.bottom, 50),
+        alignment: .bottom
+      )
+  }
 }
 
-struct SwiftUIView_Previews: PreviewProvider {
-    static var previews: some View {
-        SwiftUIView()
-    }
+struct CardView_Previews: PreviewProvider {
+  static var previews: some View {
+    CardView(honeymoon: honeymoonData[0])
+      .previewLayout(.fixed(width: 375, height: 600))
+  }
 }
