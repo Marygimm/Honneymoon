f61d71ff8aa01c14b8cd0d4277e7fb6b194a5959
Marygimm <marygimm.moreira@bringglobal.com>
2022-10-28 12:25:31 +0000

    play sound func

---
+++ Honeymoon/Helper/PlaySound.swift
@@ -0,0 +1,21 @@
+//
+//  PlaySound.swift
+//  Honeymoon
+//
+//  Created by Mary Moreira on 27/10/2022.
+//
+
+import AVFoundation
+
+var audioPlayer: AVAudioPlayer?
+
+func playSound(sound: String, type: String) {
+  if let path = Bundle.main.path(forResource: sound, ofType: type) {
+    do {
+      audioPlayer = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: path))
+      audioPlayer?.play()
+    } catch {
+      print("ERROR: Could not find and play the sound file!")
+    }
+  }
+}
